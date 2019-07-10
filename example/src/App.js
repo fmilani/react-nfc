53a17bea56d1f021d9847286dcf683e441e1398b
Felipe Milani <felmilani@gmail.com>
2019-07-09 22:31:45 -0300

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-import ExampleComponent from 'react-nfc'
+import { useNfcRead } from 'react-nfc'
 
-export default class App extends Component {
-  render () {
-    return (
+function Read({ timeout }) {
+  const data = useNfcRead(timeout)
+  return (
+    <div>
       <div>
-        <ExampleComponent text='Modern React component module' />
+        Status: {data.status}.
+        {data.status === 'READY' ? <span>Time: {data.time}</span> : null}
+        {data.status === 'UNAVAILABLE' ? (
+          <h3>Nfc disabled for this device</h3>
+        ) : null}
       </div>
-    )
-  }
+      <div>Data: {JSON.stringify(data.data)}</div>
+      <button
+        onClick={() => {
+          console.log(data.retry)
+          data.retry()
+        }}
+      >
+        Retry
+      </button>
+    </div>
+  )
 }
+
+function App() {
+  const [isReading, setIsReading] = useState(false)
+  const READ_TIMEOUT = 10
+  return (
+    <div>
+      <h1>Example react-nfc app</h1>
+      <button onClick={() => setIsReading(true)}>Read</button>
+      <button onClick={() => setIsReading(false)}>Stop Read</button>
+      {isReading ? <Read timeout={READ_TIMEOUT} /> : null}
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,84 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
 
-import styles from './styles.css'
+function noop() {}
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+const stopWatching = watchId => {
+  navigator.nfc
+    .cancelWatch(watchId)
+    .then(() => {
+      console.log('Stopped watching for a nfc tag.')
+    })
+    .catch(error => {
+      if (error.name === 'NotFoundError') {
+        // we're ignoring NotFoundError, assuming it happened because the
+        // watch was already cancelled after a write
+        return
+      }
+      throw error
+    })
 }
+
+function nfcAvailable() {
+  if (navigator.nfc) {
+    return true
+  }
+  return false
+}
+
+function read(setResponse = noop, timeout) {
+  if (!nfcAvailable()) {
+    setResponse(prevState => ({
+      ...prevState,
+      status: 'UNAVAILABLE',
+    }))
+    return
+  }
+  setResponse(prevState => ({
+    ...prevState,
+    status: 'INITIALIZING',
+  }))
+  navigator.nfc
+    .watch(message => {
+      setResponse(prevState => ({
+        ...prevState,
+        data: message.records[0].data,
+      }))
+    })
+    .then(watchId => {
+      const timeoutId = setTimeout(() => {
+        stopWatching(watchId)
+        setResponse(prevState => ({
+          ...prevState,
+          status: 'TIMEOUT',
+        }))
+      }, timeout * 1000)
+      const intervalId = setInterval(() => {
+        setResponse(prevState => ({
+          ...prevState,
+          time: prevState.time - 1,
+        }))
+      }, 1000)
+      setResponse(prevState => ({
+        ...prevState,
+        status: 'READY',
+        time: timeout,
+        retry: () => {
+          clearTimeout(timeoutId)
+          clearInterval(intervalId)
+          stopWatching(watchId)
+          read(setResponse, timeout)
+        },
+      }))
+    })
+}
+
+function useNfcRead(timeout = 15) {
+  const [response, setResponse] = useState({ retry: noop })
+  useEffect(() => {
+    read(setResponse, timeout)
+    return () => stopWatching()
+  }, [])
+  return response
+}
+
+export { useNfcRead }
